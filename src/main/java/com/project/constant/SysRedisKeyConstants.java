fe40ff6bee2ff17741abdec51d66f4968bb9a436
Destiny-wei <1342348280@qq.com>
2022-08-30 17:43:36 +0800

    新增字典类型,字典数据接口(未测试)

---
+++ src/main/java/com/project/constant/SysRedisKeyConstants.java
@@ -0,0 +1,18 @@
+package com.project.constant;
+
+/**
+ * @author : wangqing
+ * @description :
+ * @create :  2021/07/06 16:01
+ **/
+public interface SysRedisKeyConstants {
+    /**
+     * 参数管理 cache key
+     */
+    String SYS_CONFIG_KEY = "sys_config:";
+
+    /**
+     * 字典管理 cache key
+     */
+    String SYS_DICT_KEY = "sys_dict:";
+}
---
+++ src/main/java/com/project/mapper/SysDictDataMapper.java
@@ -0,0 +1,15 @@
+package com.project.mapper;
+
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.project.model.entity.SysDictDataEntity;
+
+/**
+ * 字典表 数据层
+ *
+ * @author smalljop
+ */
+public interface SysDictDataMapper extends BaseMapper<SysDictDataEntity> {
+
+
+}
